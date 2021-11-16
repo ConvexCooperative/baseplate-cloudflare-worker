2d22e451251585ff5070b9ae7a9e0494c1e73d71
joeldenning <joeldenning@gmail.com>
2021-11-15 17:20:19 -0700

    Add tests for handleImportMap.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import('@types/babel__core').TransformOptions} */
 const config = {
   presets: ["@babel/preset-typescript"],
+  env: {
+    test: {
+      presets: ["@babel/preset-typescript"],
+    },
+  },
 };
 
 module.exports = config;
