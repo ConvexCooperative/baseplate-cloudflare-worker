3439a38501d79ca93fbb9b27422553d6f01e8d9a
joeldenning <joeldenning@gmail.com>
2022-02-21 17:21:19 -0700

    Self review

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,2 +1,5 @@
 declare var MAIN_KV: KVNamespace;
+
+// Cloudflare worker env variables
+// https://developers.cloudflare.com/workers/platform/environment-variables
 declare var FOUNDRY_MFE_HOST: string;
