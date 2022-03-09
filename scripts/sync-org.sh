d76fc9df743078cfe0f82aa4c25f811ccce23a1a
Ethan Karp <ethansmainemail@gmail.com>
2022-03-08 22:47:32 -0700

    updated script to create kv entries for all environments besides non-preview prod

--- scripts/sync-org.sh
+++ scripts/sync-org.sh
@@ -6,6 +6,15 @@ then
   exit 1
 fi
 
+sync_env () {
+  echo "Syncing ${@:2}"
+  pnpm exec wrangler kv:key put org-settings-$1 '{"orgExists": true}' --binding MAIN_KV ${@:2}
+  pnpm exec wrangler kv:key put import-map-$1-__main__-systemjs '{"imports": {"react": "https://cdn.single-spa-foundry.com/react.js"}, "scopes": {}}' --binding MAIN_KV ${@:2}
+}
+
 echo "Syncing org $1"
-pnpm exec wrangler kv:key put org-settings-$1 '{"orgExists": true}' --binding MAIN_KV
-pnpm exec wrangler kv:key put import-map-$1-systemjs '{"imports": {"react": "https://cdn.single-spa-foundry.com/react.js"}, "scopes": {}}' --binding MAIN_KV
+sync_env $1 --env dev --preview
+sync_env $1 --env dev
+sync_env $1 --env test --preview
+sync_env $1 --env test
+sync_env $1 --preview
