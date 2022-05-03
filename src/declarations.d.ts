eeceffd4e4647c17e339b339aa16e68f43650458
Ethan Karp <ethansmainemail@gmail.com>
2022-05-03 10:23:34 -0600

    secrets

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,8 @@
 declare var MAIN_KV: KVNamespace;
 declare var FOUNDRY_ENV: string;
+declare var S3_PROXY_REGION: string;
+declare var S3_PROXY_ACCESS_KEY_ID: string;
+declare var S3_PROXY_SECRET_ACCESS_KEY: string;
 
 // Cloudflare worker env variables
 // https://developers.cloudflare.com/workers/platform/environment-variables
